f080fcfa368455c68757b1ff49f7ae4bd5ff24d6
isdemidoff <isdemidoff@gmail.com>
2025-12-17 13:59:23 +0700

    Day 3 of 2025

---
+++ AOC2025/Day3/src/main/kotlin/SolutionOfDay3.kt
@@ -0,0 +1,14 @@
+import kotlin.math.max
+
+fun solveForFileName(fileName: String) = readLines(fileName)
+    .sumOf { findMaxOutputJoltage(it) }
+
+fun findMaxOutputJoltage(battery: String): Int {
+    var max = 0
+    battery.forEachIndexed { leftIndex, leftChar ->
+        battery.drop(leftIndex + 1).forEach { rightChar ->
+            "$leftChar$rightChar".toInt().also { max = max(max, it) }
+        }
+    }
+    return max
+}
--- AOC2025/Day3/src/test/kotlin/SolutionOfDay3Test.kt
+++ AOC2025/Day3/src/test/kotlin/SolutionOfDay3Test.kt
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Test
 
-class SolutionOfDay2Test {
+class SolutionOfDay3Test {
     @Test
     fun showAnswer() {
         println(solveForFileName("input.txt"))
