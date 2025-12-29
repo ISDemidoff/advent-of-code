ae9041e6b823e6acb4da1dea27450e746001e666
isdemidoff <isdemidoff@gmail.com>
2025-12-29 20:59:41 +0700

    Day 9 solution

---
+++ AOC2025/Day9/src/main/kotlin/SolutionOfDay9.kt
@@ -0,0 +1,27 @@
+import Point.Companion.toPoint
+import isdemidoff.utility.cartesianProduct
+import isdemidoff.utility.input.readLines
+import isdemidoff.utility.parseUnescapedCsvInputLines
+import kotlin.math.absoluteValue
+
+data class Point(val x: Long, val y: Long) {
+    infix fun rectangleSizeWith(other: Point) = sideLength(this.x, other.x) * sideLength(this.y, other.y)
+
+    companion object {
+        fun List<Long>.toPoint(): Point {
+            require(this.size == 2) { "Expected exactly 2 coordinates but got ${this.size}" }
+            return Point(this[0], this[1])
+        }
+    }
+}
+
+fun sideLength(first: Long, second: Long) = (first - second).absoluteValue + 1
+
+fun solveForFileName(fileName: String) = readLines(fileName)
+    .parseUnescapedCsvInputLines { it.toLong() }
+    .map { it.toPoint() }
+    .let { findMapRectangleSize(it) }
+
+fun findMapRectangleSize(points: List<Point>) = cartesianProduct(points, points) { (first, second) -> first rectangleSizeWith second }.max()
+
+
